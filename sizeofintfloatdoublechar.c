8d4bc1bfa43a7c42725f931e463a3b95c00c9f9f
tjmahir609-xyz <tj.mahir.609@gmail.com>
2026-04-18 04:33:17 +0600

    Add files via upload
    Sizeof integer float double character

---
+++ sizeofintfloatdoublechar.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+
+int i;
+float f;
+double lf;
+char c;
+
+printf("size of int=%d\n",sizeof(i));
+
+printf("size of float=%d\n",sizeof(f));
+
+
+printf("size of double=%d\n",sizeof(lf));
+
+
+printf("size of char=%d\n",sizeof(c));
+
+
+return 0;
+
+}
