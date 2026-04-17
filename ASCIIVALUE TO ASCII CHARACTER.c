b618ee051b8e75251217f3495bc2d49420f53f4e
Mahir Tajwar <tj.mahir.609@gmail.com>
2026-04-18 05:38:15 +0600

    ASCII VALUE TO ASCII CHARACTER
    ASCII VALUE TO ASCII CHARACTER

---
+++ ASCIIVALUE TO ASCII CHARACTER.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+
+int asciivalue1,asciivalue2,asciivalue3;
+
+printf("Enter 1st 2nd and 3rd ASCII Value:");
+
+scanf("%d""%d""%d",&asciivalue1,&asciivalue2,&
+asciivalue3);
+
+
+printf("The 1st ASCII Charactes is: %c\n",asciivalue1);
+printf("The 2nd ASCII Charactes is: %c\n",asciivalue2);
+printf("The 3rd ASCII Charactes is: %c",asciivalue3);
+
+return 0;
+
+}
